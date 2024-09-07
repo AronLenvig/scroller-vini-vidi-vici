2181744d0d6a757de87b895672ba5e30f0f5f458
AronLenvig <AronLenvigg@gmail.com>
2024-09-07 19:57:55 +0100

    adf

--- scripts/add_point.gd
+++ scripts/add_point.gd
@@ -4,7 +4,6 @@ extends Node2D
 @export var _action: Callable
 
 var velocity = Vector2()
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
