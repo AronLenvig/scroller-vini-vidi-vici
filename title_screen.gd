45c626383158f3ed9b9987ae85fafa484837e57c
Johann <johannlenvig@hotmail.com>
2024-09-07 14:36:36 +0100

    rone

--- title_screen.gd
+++ title_screen.gd
@@ -2,7 +2,9 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	Transition.transition()
+	await Transition.on_tansition_finished
+	get_tree().change_scene_to_file("res://intro_cutscenes.tscn")
 
 
 func _on_quit_button_pressed() -> void:
