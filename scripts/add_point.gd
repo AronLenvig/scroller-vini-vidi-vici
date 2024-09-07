0912df39ea279d36c01515140f416d2121c632e1
AronLenvig <AronLenvigg@gmail.com>
2024-09-07 16:44:10 +0100

    random point gain

--- scripts/add_point.gd
+++ scripts/add_point.gd
@@ -1,5 +1,10 @@
 extends Node2D
+
+@export var label_text: String
+@export var _action: Callable
+
 var velocity = Vector2()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,6 +12,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var label = $Area2D/Label
+	label.text = label_text
+	
 	velocity.y = Global.game_speed  # Change the speed of the movement here
 	position.y += velocity.y * delta
 	# if object is more than 2000 delete it
@@ -15,6 +23,5 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		# Signal the player or global manager to increase points
-		Global.soldiers += 100
+		_action.call()
 		queue_free()  # Remove this node (the spawned object)
--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -5,6 +5,13 @@ var add_point_node = preload("res://Sceans/add_point.tscn")
 var time_passed: float = 0.0
 var increment_interval: float = 1.0
 
+var lable_funct = [
+	["+100", "add100"],
+	["-100", "sub100"],
+	["*2", "ganga2"],
+	["/2", "div2"],
+	["-69", "sub69"],
+]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -19,5 +26,26 @@ func _process(delta: float) -> void:
 func spawn_point_gain(pos: Vector2):
 	var point_instance = add_point_node.instantiate()
 	#add_point_node.positions = pos
+	var _item = lable_funct.pick_random()
 	point_instance.position = pos
+	point_instance.label_text = _item[0]
+	var callable = Callable(self, _item[1])
+	point_instance._action = callable
+	
 	add_child(point_instance)
+
+func add100():
+	Global.soldiers += 100
+	
+func sub100():
+	Global.soldiers -= 100
+	
+func ganga2():
+	Global.soldiers = Global.soldiers * 2
+	
+func div2():
+	Global.soldiers = Global.soldiers / 2
+	
+func sub69():
+	Global.soldiers -= 69
+	
