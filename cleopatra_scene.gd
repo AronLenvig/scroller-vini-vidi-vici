9f99ad284d9e4e052efac0ce6d4ac3ea89ea5bdb
AronLenvig <AronLenvigg@gmail.com>
2024-09-08 15:50:00 +0100

    asfdasfd

---
+++ cleopatra_scene.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+func _on_button_pressed() -> void:
+	Transition.transition()
+	await Transition.on_tansition_finished
+	get_tree().change_scene_to_file("res://Sceans/intro_cutscenes.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
