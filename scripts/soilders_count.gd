e6bee20e29f47388fb510663fe3b775909438deb
AronLenvig <AronLenvigg@gmail.com>
2024-09-08 12:24:09 +0100

    asd

--- scripts/soilders_count.gd
+++ scripts/soilders_count.gd
@@ -2,7 +2,10 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+	# play sound from loca
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
