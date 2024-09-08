3fee2348fab89c10efab68d145ce217ce9d59f7e
AronLenvig <AronLenvigg@gmail.com>
2024-09-08 12:13:53 +0100

    fadsd

--- death_screen.gd
+++ death_screen.gd
@@ -1,7 +1,12 @@
 extends Control
 
+func _ready() -> void:
+	var sound = $AudioStreamPlayer
+	sound.play()
 
 func _on_button_pressed() -> void:
+	var sound = $AudioStreamPlayer
+	sound.stop()
 	Transition.transition()
 	await Transition.on_tansition_finished
 	get_tree().change_scene_to_file("res://Sceans/intro_cutscenes.tscn")
